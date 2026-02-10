09630a3fd86ecd9a1cb348fad1de2bac29bfe68c
m-lois-bl <marion.blayo2023@campus-eni.fr>
2026-02-10 16:18:40 +0100

    Test changement méthode Snetry

--- src/components/SentryTestComponent.tsx
+++ src/components/SentryTestComponent.tsx
@@ -3,8 +3,11 @@ import Button from "./Button";
 export default function SentryTestComponent() {
       const handleCrash = () => {
         // Cette fonction va planter car 'undefined' n'a pas de propriété 'name'
-        const user: any = undefined;
-        console.log(user.name);
+        // const user: any = undefined;
+        // console.log(user.name);
+        console.log("Erreur test Sentry")
+        throw new Error("Sentry Test Error");
+
       };
 
       return <Button onClickFn={handleCrash} text="Sentry Test"/>;
