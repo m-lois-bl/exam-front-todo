a2199dfceed8b288a23fa1faab27498dc430b150
m-lois-bl <marion.blayo2023@campus-eni.fr>
2026-02-10 18:31:00 +0100

    Ajout config plugin Sentry.

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   
   plugins: [
     tailwindcss(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    sentryVitePlugin({
+      org: "your-org",
+      project: "your-project",
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+
+      reactComponentAnnotation: {
+        enabled: true,
       },
     }),
   ],
